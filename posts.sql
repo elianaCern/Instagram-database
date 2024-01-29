b5b6b9a792cb6f077c82999c5a2168a80a1d216f
elianaCern <eliana.cernevic@gmail.com>
2024-01-29 13:07:46 +0200

    added table posts

---
+++ posts.sql
@@ -0,0 +1,140 @@
+insert into posts (url, caption, user_id)
+values 
+ ('https://sogou.com', 'sapien ut nunc vestibulum ante ipsum primis inilia curae mauris', 35),
+ ('https://amazon.co.jp', 'pellentesque volutpat dui maecenas tristiqueposuere cubi ipsum aliquam', 42),
+ ('https://gov.uk', 'tempus sit amet sem fusce consequat nulla nisl nuit eu est congs eget elit', 23),
+ ('http://cnbc.com', null, 33),
+ ('https://apple.com', 'sed nisl nunc rhoncus dui vel sem sstibulumociis natoque penatibus et magnis dis parturient 
+  montes nascetur ridiculus mus etiam vel augo morbi non ligula ieis congue vivamus metus arcu adipiscing molestie hendrerit
+  at vulputate vitae nisl aenean lectus pelidunt in leo maecenas pt vulputate ut', 30),
+ ('http://tamu.edu', 'maecenas leo odio condi pellentea magna ac consequati non lectus
+  aliquam sit amet diam in magna bibagittis la sinulis natoque penatibus et 
+  magnis dis parturient montes nascetur ridiculusrturient montes nascetur ridiculus', 33),
+ ('https://columbia.edu', 'proitate  duis ac nibh fusce lacus purus aliquet at
+  feugiat non pretium quis lectus  odio in hac habugue commod blandit nam nulla integera eget tincidunt 
+  eget tempus vel pede morbi porttitor lorem', 24),
+ ('https://uol.com.br', 'vestibulum sit amet cursusis tortoras pellentesque volutpat dui maecenas tristique est et
+  tempus semper est quam pharetra magna ante ipsum punt eu felis fusce oncus dui vel sem 
+  sed sagittis nam congue risus semper porta eifend pede libero qui potenti cras in purus eu magna u dis parturient', 5),
+ ('https://discovery.com', 'nulla tell molestie lorem quisque ut erat cugue quam sollicitudin', 34),
+ ('https://ameblo.jp', 'nec molemaecenas tristiqer e mdiam in magna bibendum i
+  tincidunt eu felis fusce posuere felis sed lacus el nascetur ridiculusntes nascetur', 9),
+ ('http://nydailynews.com', 'et magnis dis us etiam veliaculis congue vivamu vitae nisl aenean lectus 
+  pellentesque eget nunc donec quis orci eget orci vante ipsum
+  primis in faucibus orci luctus et ultricuet ultrices erat', 36),
+ ('https://reddit.com', 'volutpat', 3),
+ ('http://mashable.com', null, 45),
+ ('http://un.org', 'gravida sem praesent ivallis morbi odio odio 
+  elementum eu interdum eu tincidunt in leoe quam turpis adipiscing lorem vitae mattis', 8),
+ ('https://tinypic.com', 'non quam nec dubh fnti in eleifelatante vel ipslum sed magna at nunc commodo', 43),
+ ('http://nydailynews.com', 'praesent  id salutpat convallis morbi odio odio elementum 
+  eu interdum eu tincidunt in leo maecenas pulvina loborten noluctus rutrum nulla', 4),
+ ('http://berkeley.edu', 'est et tempus et ultrices bibendum imperdiet nullam sodales sed tincidunt', 2),
+ ('https://tinyurl.com', null, 4),
+ ('http://mlb.com', null, 2),
+ ('http://issuu.com', 'erat id mauris', 30),
+ ('https://vistaprint.com', 'lectus in ea donec dapibusurpis sed ante vivamus tortor duis mattis egestas 
+  metus aenean fermentum donec ut icitudin ut suscipilum ac est lacinia nisi venenatis', 11),
+ ('http://facebook.com', 'ut massa volutps pulvinar orci luctus et ultrices posuere cubilia curae donec pharetra
+  magna vestibulum aliquet ultrices erat tortor sollicitudin mium morbi non qu suspendisse potenti in eleifend quam', 23),
+ ('http://businessinsider.com', null, 15),
+ ('http://yolasite.com', 'integer pede justo lacinia egetvenenatis ulum velien placerat ante nulla justo aliquam quis turpis
+  eget elit sodales scelerisque maurissollicitatis tristique fusce congue mauris', 29),
+ ('https://blogger.com', 'lectus pelo vulputate justo in blandit ultrices enim lorem ipsum dolor
+  sit amet consectetuer adipiscing elit psque eger in libero ut massa volutpat convallis morbi odio odio elementum eu interdum eu', 15),
+ ('https://wisc.edu', 'sed tincidunt eu felis fusce populvinar sed nisl nunc rhoncus dui vel sem', 1),
+ ('http://diigo.com', 'cum sociis natoquediculus id ncommodo elit proin interdum mauris non ligula pellentesque', 33),
+ ('http://parallels.com', 'morbi porttitor lorem idconsequat nulla nisl nunc nisl duis bibendum felis sed interdum venenatis turpis', 10),
+ ('https://blogtalkradio.com', null, 5),
+ ('https://engadget.com', 'dapibus duis at velit stibulum ante 
+  ipsum primis in faucibus orci luctus et  suscipicidunt lacus at velit
+  vivamus vel  nunc purus', 45),
+ ('https://businesswire.com', 'morbi vel lectus in quam oncus  ique est et tempus semper est quam 
+  pharetra magna ac consequat metus sapien u porttitor lacus at turpis donec posuere metus vitae ipsum aliquam non', 27),
+ ('http://tamu.edu', 'ante ipsum primis in fauc duis ac nibh fusce lacus purus aliquet at feugiat', 20),
+ ('https://gnu.org', 'ut dolor morbi vel lectus in quam fringilla uam pharetra 
+  magna ac consequat metus sapien ut nunc vestibulum ante i bibendum imperdiet nullam orcifelis sed lacus', 5),
+ ('http://github.io', 'mattis nibh ligula nauguecurae feugiat non pretium quis lectus suspendis eu est congue', 45),
+ ('https://businesswire.com', 'donec ut dolor morbi vel lectus in msan mleo odio condimentum id luctus nec molestie sed justo', 16),
+ ('https://ihg.com', 'vestibulum vestibulum ante ipsum primis', 48),
+ ('https://harvard.edu', 'luctus tincidunt nulla mollis molestt aliquas sed interdum venenatis turpis 
+  enim blandit mi in porttitor peis mattis egeneque libero convallis eget eleifend luctus ultricies eu nibh quisque id jus angue aliquam', 31),
+ ('https://wsj.com', null, 4),
+ ('http://livejournal.com', 'sapien iaculis congue vivamus metucidunt ante vel ipsum praeseinia eratulla integer  tincidunt eget', 7),
+ ('https://elpais.com', null, 50),
+ ('https://huffingtonpost.com', null, 16),
+ ('https://twitpic.com', 'id nisl venenatis lacinia aenean siproin interdum mauriscongue vivamus metus', 13),
+ ('https://boston.com', 'sit amet diam in magna bibendubulum sagittis sapien cum sociis natoque pen 
+  dis parturient montes nascetur ridiculus mus etiam vel augue neque aenean auctor gravida sem praesent id massa', 16),
+ ('http://msn.com', 'diam vitae quam suspendisse potenti nullam poui vel sem sed sagittis nam congue risus semper', 19),
+ ('https://chronoengine.com', 'orci luctus et ultriceui nec nistiula consequat morbi a ipsum integer a', 25),
+ ('https://foxnews.com', null, 7),
+ ('http://fema.gov', 'magna at nunc commodo placerat praesent blandit nam nulla', 48),
+ ('https://elpais.com', 'nunc donec quis orci eget orci vehicula condimrbi odpulvinar lobortis est phasellus sit amet erat nulla', 49),
+ ('http://economist.com', null, 15),
+ ('http://seattletimes.com', null, 22),
+ ('http://soup.io', 'nulla ut erat id mauris vulputate elementtis pulvinar nulla pede ullamcorper', 41),
+ ('https://ucoz.ru', 'eget tincidunt eget tempus vel pedvenenatis turpisdapibus platea dictumst diam erat fermentum justo nec condimentum neque', 38),
+ ('http://surveymonkey.com', 'tincidunt lacus at velit vivamus vel', 42),
+ ('https://aboutads.info', 'neque duis bibenandit nam nulla integer pede justo 
+  lacinia eget tincidunt eget tempus vel pede mblandit mi in porttitor pede justo eu massa donec dapibus duis at velit', 32),
+ ('https://utexas.edu', 'mi in porttitor pede  hac habitasmentum aurvivamultriclum vestibulum ante ipsum primis in faucibus orci luctus et
+  ultrices posuere cubilia curae nulla dapibus dolor vel estulutpat eiverra eget congue eget', 11),
+ ('http://dropbox.com', 'arcu sed augue aliquam platea  phasellus in felis donec semper sapien a libero nam dui proin leo odio porttitor', 19),
+ ('https://reuters.com', 'quisque ut erat blandit lacinia erat vestibulum sed magna at nunc commodo placerat praesent', 1),
+ ('http://prnewswire.com', 'nulla mollis e justo eu massa donec 
+  dapibus duis at velit eu est congue elementum in hac habitasse plateaor convallis nulla neque libero convallis', 15),
+ ('http://wisc.edu', null, 18),
+ ('https://rediff.com', 'elementum ligula vehicula s dio condimentum id luctus nec molestie sed justo pellentesque viverra pede ac', 10),
+ ('https://hibu.com', 'sociis nbl sapien in sapien iaculis congue vivamus metus arcu adipiscing molestie hendrerit at vulputate vitae 
+  nisl aenean lectus  turpis  diam neque vestibuluputate ut ultrices ibulum ante cibus orci luctus et ultrices posuere', 10),
+ ('http://netscape.com', 'sed lacusdui cras in purus eu magna vulputate luctus cum sociis natoque', 5),
+ ('https://bravesites.com', 'nec euismod scelerisproin a magna vestibulum aliquet', 34),
+ ('https://jalbum.net', 'mattis nibhagittis dui vel nisl duis ac nibh fusce lacus purus aliquet atn eleifend quam', 26),
+ ('http://smh.com.au', 'ut nunc vestibulum ante ipsendectus aliqporta nibh in lectus pellentesque at nulla suspendisse potenti 
+  cras in purus eu magna vulputate luctus cumphasellus id sapien in sapien iaculis', 50),
+ ('http://dagondesign.com', null, 46),
+ ('https://kickstarter.com', 'placerat praesent blandir eget elit sodales scelerisque mauris sit amet eros rtor quis 
+  turpis sed ante vivamus tortor duis mla neque amet  sed quet pulvinar sed nisl nunc rhoncus 
+  dui vel sem sed sagittis nam congue risus sempeavida sem praesent id massa id nisl venent commodo vulputate justo', 11),
+ ('https://state.tx.us', 'sed interdum ven libero convallis e sit amet sapien diin faucibus orci luctus
+  et ultrices posuere cubilia curae nulla dapiate no eget semper rutrum nulla nunc', 25),
+ ('http://cdc.gov', 'vivamus in felis eu sapien cursus vestibulum proin eu mi nule duis dui vel nisl duis ac nibh fusce lacus pum 
+  quis lectus suspendisse potenti in eleifend quam a odio in hac habiida sed magna', 1),
+ ('http://fda.gov', 'condimentum curabitur in libero ut mi nulla ac enmet lobortis sapien sapien 
+  non mi integer ac neque duis bibendum morbi non quam nec dui luctus ut massa quis augue luctus tincidunt', 37),
+ ('https://dot.gov', 'sollicitudin ut suscipit a fenisi veisl utiaus urna ut tellus nulla ut', 15),
+ ('https://tamu.edu', 'ut suscipit autpat sapien arcuatemei proin leo odio
+  porttitor id consequat in consequat ut nulla sed accumsan felis ut at dolor quis odio conar nulla pede ullamcorper augue', 25),
+ ('http://cnet.com', 'augue luctus tincidunt nulla mollis molestie lorem', 48),
+ ('http://dyndns.org', 'dui maecenas tristique est et tempusm sed rtisero quis orci nullam molestie
+  nibh in lectus pellentesque at nulla suspendisse po in blandit ultrices enim lorem', 50),
+ ('https://illinois.edu', 'vestibulum sed litentumodales scelerisque suspendisse accumsan tortor quis turpis
+  sed ante vivamus tortor duis mattis egestas metus aenean fermentum donecpm vestibulum ante ipsum primis in', 17),
+ ('https://cocolog-nifty.com', 'scelerisque mauum nisl ut volutpat sapienetium iaculis justo in hac 
+  habitasse platea dictumst etiam faucibus curin felis donec sempe', 10),
+ ('https://nifty.com', null, 41),
+ ('http://google.com', 'pede libero quis orci nulla nisl venenatis lacinia aenean sit amet', 47),
+ ('https://wikimedia.org', 'pellentesque eget nunc donec quis orci eget orci vehicula condimentum curabitur in libero ut massa', 10),
+ ('https://goodreads.com', 'curabitur in libero ut massa volutpat convallis morbi odio odio elementum eu', 3),
+ ('https://springer.com', 'platea dictumst maecenas ut massa quis augue luctus tincidunt nulla mollis molestie lorem quisque ut', 49),
+ ('http://ezinearticles.com', 'nulla mollis mt tempm in hac habitasse platea dictumst morbi 
+  vestibulum velit id pretium iaculis diam erat fermentum justo to aliquam quis turpis eget elit sodales', 13),
+ ('http://bandcamp.com', 'fringilla rhoncus ristique est in faucibus orci luctus et ultrices posuere cubilia curae mauris viverra diam vitae quam 
+  suspendisse potenti nullam porttitor lac laoreet ut rhontie nibh in lectus pellentesque at nulla n pulputate luctus', 8),
+ ('http://princeton.edu', 'amet consectetuer adiivamus metus arcu adipiscing moeget convallis morbi odio odio elementum', 14),
+ ('http://twitter.com', 'luctus et ultrices posuere cubilia curae duis faucibus accumsan odio curabitur c lectus in quam fringilla rhoncus', 26),
+ ('https://psu.edu', 'montes nascetut orci vehicula condimentum curabitur in libero ut massa volutpat 
+  convallis morbi odio odio elementum eu interdum eu tincidunt in leo maecementum neque sapien placerat ante nulla justo aliquam quis turpis eget elit', 1),
+ ('http://salon.com', null, 16),
+ ('https://sbwire.com', 'vulputate justo in blandit ultrices epien iaculis congue vivamus metus arcu adipiscing', 19),
+ ('https://posterous.com', 'erat tortor sollicitudin mi eger pede justo lacinia 
+  eget tincidunt eget tempus vel pede morbi porttitor loreandit mi in porttitor pede justo eu massa donec dapibus duis', 29),
+ ('http://dot.gov', null, 40),
+ ('http://unblog.fr', null, 50),
+ ('http://icio.us', 'in imperdiet et it proin interdum mauris e sapien cursus vestibulum proin eu mi nadipiscing lorem vitae mattis nibh', 2),
+ ('https://gmpg.org', null, 6),
+ ('http://wikia.com', 'nulla suscipit ligula in lacus curabitur at ipsum ac tellus semper interdum mauris', 36),
+ ('https://hubpages.com', 'quis turpis eget elit soda imperdiet sapien urna psto etiam pretium 
+  iaculis justo in hac habitasse platea dictumst etiamaeunc itor id consequat in consequat ut nulla sed accumsan f  integer i na non mattis pulvinar', 46);
+
